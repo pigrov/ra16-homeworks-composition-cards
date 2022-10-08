fae823290cf01929a93e96e854fb844fb7f7188e
Dmitry Pigrov <pigrov@gmail.com>
2022-10-08 07:57:24 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Cards from "./components/Cards";
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <Cards title="Card Title">
+    <Cards
+      title="Card Title"
+      text="Some quick example text to build on the card title and make up the bulk of the card's content."
+    >
       <Card.Img
         variant="top"
         src="https://s1.1zoom.ru/big0/94/345461-admin.jpg"
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,10 +8,7 @@ export default function Cards(props) {
         {props.children}
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Text>{props.text}</Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
